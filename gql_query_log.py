ef29634dfaa0fc8ef6f6bc94267e3b47f2394a98
Ross Barnowski <rossbar@berkeley.edu>
2019-12-20 12:44:14 -0800

    Modified log: lstrip not necessary for json payload

--- gql_query_log.py
+++ gql_query_log.py
@@ -9,10 +9,10 @@ endpoint = r'https://api.github.com/graphql'
 headers = {'Authorization': 'bearer {}'.format(token)}
     
 # Get a sample gql query
-with open('query_examples/num_crossreferences_in_first_100_issues.json', 'r') as ff:
+with open('query_examples/open_issue_crossrefs.gql', 'r') as ff:
     req_data = ff.read()
 # Turn loaded query into a properly-formatted json payload
-payload = {'query' : ''.join(map(str.lstrip, req_data.split('\n')))}
+payload = {'query' : ''.join(req_data.split('\n'))}
 
 # Send request
 response = requests.post(endpoint, json=payload, headers=headers)
@@ -20,6 +20,7 @@ print(response.status_code)
 data = json.loads(response.content)
 data = data['data']['repository']['issues']['edges']
 
+# Example analysis: look at distribution of number of connections per node
 import numpy as np
 refcounts = np.array([line['node']['timelineItems']['totalCount'] for line in data], dtype=int)
-np.bincount(refcounts)
+print(np.unique(refcounts, return_counts=True))
