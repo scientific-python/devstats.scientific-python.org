620c3b48a7f7ab72d02e046db28afccf7993428c
Ross Barnowski <rossbar@berkeley.edu>
2020-01-07 22:23:46 -0800

    Modified issues.py and queries to make more general.
    Still some pieces that rely on specific structure, but
    now should be able to call any issues query that has a
    totalCount of the number of issues and a cursor.

--- issues.py
+++ issues.py
@@ -6,16 +6,17 @@ token = os.environ['GRAPH_API_KEY']
 endpoint = r'https://api.github.com/graphql'
 headers = {'Authorization': 'bearer {}'.format(token)}
 
-def get_open_numpy_issues_with_crossrefs(cursor=None):
+def send_query(query_fname, cursor=None):
     """
     Helper function to use the graphql query example in `query_examples`
     to retrieve open numpy issues and all cross references
     """
     # Load request from examples
-    with open('query_examples/open_issue_crossrefs.gql', 'r') as ff:
+    with open(query_fname, 'r') as ff:
         req_data = ff.read()
     # Modifications to request template
     # TODO: Unhack this
+    # WARNING: This hack relies on specific structure of issues query
     if cursor is not None:
         cursor_ind = req_data.find('OPEN') + len('OPEN')
         req_data = req_data[:cursor_ind] + ' after:"{}"'.format(cursor) + req_data[cursor_ind:]
@@ -24,18 +25,21 @@ def get_open_numpy_issues_with_crossrefs(cursor=None):
     response = requests.post(endpoint, json=payload, headers=headers)
     return json.loads(response.content)
 
-def get_all_open_issues():
+def get_all_responses(query_fname):
     """
     Helper function to bypass GitHub GraphQL API node limit.
     """
     # Get data from a single response
-    inital_data = get_open_numpy_issues_with_crossrefs()
+    inital_data = send_query(query_fname)
     data, last_cursor, total_num_issues = parse_single_issue_query(inital_data)
+    print("Retrieving {} out of {} values...".format(len(data), total_num_issues))
     # Continue requesting issues (with pagination) until all are acquired
     while len(data) < total_num_issues:
-        rdata = get_open_numpy_issues_with_crossrefs(cursor=last_cursor)
+        rdata = send_query(query_fname, cursor=last_cursor)
         pdata, last_cursor, _ = parse_single_issue_query(rdata)
         data.extend(pdata)
+        print("Retrieving {} out of {} values...".format(len(data), total_num_issues))
+    print("Done.")
     return data
 
 def parse_single_issue_query(data):
