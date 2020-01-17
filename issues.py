cf57f8ad685da965df9c51051b1e7d0fa8e569c0
Ross Barnowski <rossbar@berkeley.edu>
2020-01-17 11:02:26 -0800

    Added a convenience function to summarize issue query results

--- issues.py
+++ issues.py
@@ -51,5 +51,51 @@ def parse_single_issue_query(data):
     last_cursor = data[-1]['cursor']
     return data, last_cursor, total_num_issues
 
+def generate_top_issues_summary(data=None, num_issues=10):
+    """
+    Generate a markdown-formatted table of NumPy issues sorted by 
+    cross-reference count.
+
+    Parameters
+    ----------
+    num_issues : int
+        Number of issues to include in the summary table. Default = 10.
+    data : query_result
+        Result of an issue query. If :None:, the query is performed.
+
+    Returns
+    -------
+    table : str
+        A string containing a markdown-formatted table with the issue number,
+        number of cross-references, and issue title/url.
+    """
+    if data is None:
+        data = get_all_responses(
+            'query_examples/totalCount_openIssue_crossrefs_withIssueData.gql'
+        )
+
+    # Parse data into dict of dicts with key=issue number
+    node_data = {
+        n['node']['number'] : {
+            "title"   : n['node']['title'],
+            "url"     : n['node']['url'],
+            "numrefs" : n['node']['timelineItems']['totalCount']
+        } for n in data
+    }
+
+    # Initialize table
+    mdtable =  '|Iss. \#| xrefs | Issue |\n'
+    mdtable += '|:-----:|:------|:------|\n'
+
+    # Sort data by num xrefs and generate summary
+    for node in sorted(node_data.items(), key=lambda x: x[1]['numrefs'], reverse=True)[:num_issues]:
+        mdtable += '|{}|{}|[{}]({})\n'.format(
+            node[0],
+            node[1]['numrefs'],
+            node[1]['title'],
+            node[1]['url']
+        )
+    return mdtable
+
 if __name__ == "__main__":
-    client = QueryClient()
+    pass
