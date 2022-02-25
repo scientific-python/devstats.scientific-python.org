be371e7d2a5f1c4753f0e449c0aeeb8e56211edf
Ross Barnowski <rossbar@berkeley.edu>
2022-02-25 13:23:10 -0800

    Prevent README and devstats-data from being picked up by sphinx.

--- conf.py
+++ conf.py
@@ -38,7 +38,14 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "analysis_template.md"]
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+    "analysis_template.md",
+    "README.rst",
+    "devstats-data/*"
+]
 execution_excludepatterns = ["analysis_template.md"]
 
 
